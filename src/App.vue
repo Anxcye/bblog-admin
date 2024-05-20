4d548a05c2141016f44e0d9b3a8efdfc57c9c77b
Anxcye <axyyu@outlook.com>
2024-05-20 10:50:29 +0800

    feat: login page

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,6 @@ console.log(userStore)
 
 <template>
   <router-view />
-
-  <div>app</div>
-  <el-button @click="$router.push('/home')">home</el-button>
-  <el-button @click="$router.push('/list')">list</el-button>
-
-  <div>
-    <h2>{{ userStore.token }}</h2>
-    <el-button @click="userStore.setToken('asdfqwer')">log in</el-button>
-    <el-button @click="userStore.removeToken()">log out</el-button>
-  </div>
 </template>
 
 <style scoped></style>
